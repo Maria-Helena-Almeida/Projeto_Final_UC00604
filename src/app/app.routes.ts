6ff1f068771c02b1cce44b1deeefb96a1aeda12d
Maria-Helena-Almeida <mariahelena.nena@hotmail.com>
2026-01-13 21:20:12 +0000

    preencher conteudo do home e criar rotas para as paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // Página inicial
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.Home),
+  },
+  {
+    path: 'services', // Página de serviços
+    loadComponent: () =>
+      import('./pages/services/services').then(m => m.Services),
+  },
+  {
+    path: 'gallery', // Página da galeria
+    loadComponent: () =>
+      import('./pages/gallery/gallery').then(m => m.Gallery),
+  },
+  {
+    path: 'booking', // Página do formulário
+    loadComponent: () =>
+      import('./pages/booking/booking').then(m => m.Booking),
+  },
+  {
+    path: '**', // Qualquer rota inválida redireciona para a Home
+    redirectTo: '',
+  },
+];
