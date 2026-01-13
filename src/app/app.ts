759c26c92e625599541462f54021fee7a79afd5d
Maria-Helena-Almeida <mariahelena.nena@hotmail.com>
2026-01-13 20:29:14 +0000

    configurando rotas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Main } from "./components/main/main";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Header, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Projeto_Final_UC00604');
+  protected readonly title = signal('uc00604-Programa-Web');
 }
