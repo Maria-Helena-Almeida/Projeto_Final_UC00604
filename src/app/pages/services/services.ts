07efab39615c7cc8b55274125c6329d8983ae4ff
Maria-Helena-Almeida <mariahelena.nena@hotmail.com>
2026-01-13 21:23:15 +0000

    conteudo da page service

--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  standalone: true,
   templateUrl: './services.html',
-  styleUrl: './services.css',
+  styleUrls: ['./services.css']
 })
-export class Services {
-
-}
+export class Services {}
